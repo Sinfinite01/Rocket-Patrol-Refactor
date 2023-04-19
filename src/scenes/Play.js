c64e3dff67cd0f4eca5cd44adbbdc10ca4951297
Sinfinite01 <54284225+Sinfinite01@users.noreply.github.com>
2023-04-19 14:02:25 -0700

    redid code couldn't really figure out the error, but redoing fixed it

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -11,15 +11,15 @@ class Play extends Phaser.Scene {
     }
 
     create(){
-        //place tile sprite
-        this.starfield = this.add.tileSprite(0, 0, 640, 480, 'starield').setOrigin(0,0);
-        // green UI backgrounf
+        // place tile sprite
+        this.starfield = this.add.tileSprite(0, 0, 640, 480, 'starfield').setOrigin(0, 0);
+        // green UI background
         this.add.rectangle(0, borderUISize + borderPadding, game.config.width, borderUISize * 2, 0x00FF00).setOrigin(0, 0);
         // white borders
         this.add.rectangle(0, 0, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0);
         this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0);
         this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
-        this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
+        this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);    
     }
 
     update() {
